c1a7fd1d58bce8074f7f4464d4ff632c6af30c81
ZN-Qiao <qiao4zn@gmail.com>
2021-03-03 01:43:28 -0600

    --

--- train.sh
+++ train.sh
@@ -11,7 +11,7 @@ module load python/3.7.4
 module load pytorch/gpu-1.5.0
 module load cuda/101/toolkit/10.1.243
 
-CUDA_VISIBLE_DEVICES=0,1,2,3 nohup python3 ./main.py -a pynet50 ../places365_standard/
+CUDA_VISIBLE_DEVICES=0,1,2,3 nohup python3 ./main.py -a resnet50 ../places365_standard/ &
 
 
 CUDA_VISIBLE_DEVICES=0,1,2,3 python3 ./main.py -a wide_resnet50_2 ../data/places/
