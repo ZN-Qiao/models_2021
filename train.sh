65be10d381f45de7294fe8b90b6ec79082b49ab6
ZN-Qiao <qiao4zn@gmail.com>
2021-03-08 23:47:50 -0600

    --

--- train.sh
+++ train.sh
@@ -11,7 +11,7 @@ module load python/3.7.4
 module load pytorch/gpu-1.5.0
 module load cuda/101/toolkit/10.1.243
 
-CUDA_VISIBLE_DEVICES=0,1, nohup python3 ./main.py -a resnet50 --epochs 150  --resume ./checkpoint/cp_resnet50.pth.tar ../../Datasets/ImageNet &
+CUDA_VISIBLE_DEVICES=0,1, nohup python3 ./main.py -a resnet50 --epochs 150 --resume ./checkpoint/cp_resnet50.pth.tar ../../Datasets/ImageNet &
 
 
 CUDA_VISIBLE_DEVICES=0,1,2,3 nohup python3 ./main.py -a resnet50 --resume ./checkpoint/best_cp_resnet50.pth.tar ../places365_standard/ &
