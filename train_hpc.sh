916b09f264bf88db0ffb3258685dd52a612d9a0c
ZN-Qiao <qiao4zn@gmail.com>
2021-05-03 21:07:47 -0500

    --

--- train_hpc.sh
+++ train_hpc.sh
@@ -7,8 +7,7 @@
 #SBATCH -N 1
 #SBATCH --gres=gpu:4
 
-module load python/3.7.4
-module load pytorch/gpu-1.5.0
-module load cuda/101/toolkit/10.1.243
+
+
 
 CUDA_VISIBLE_DEVICES=0,1,2,3 python3 ./main.py -a resnet50 ../places365_standard/
