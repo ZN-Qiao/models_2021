149ed6ca7cb91903d14ed81edbf0c7475ece10d4
ZN-Qiao <qiao4zn@gmail.com>
2021-05-25 18:21:49 -0500

    --

--- train_1.sh
+++ train_1.sh
@@ -2,3 +2,5 @@ CUDA_VISIBLE_DEVICES=4,5,6,7 nohup python3 ./main.py -a resnet101 --resume ./che
 
 CUDA_VISIBLE_DEVICES=1,2,3,4,5,6,7 nohup python3 ./main.py -a resnet101 --resume ./checkpoint/best_cp_resnet101.pth.
 tar ../data/places &
+
+CUDA_VISIBLE_DEVICES=1,2,3,4,5,6,7 nohup python3 ./main.py -a resnet101 ../data/places &
